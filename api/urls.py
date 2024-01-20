bd97d222614c352bd937a9d5bb327dd25ade5edf
Vedant <vedantbhagwat2311@gmail.com>
2024-01-20 21:34:48 +0530

    updated api urls

--- api/urls.py
+++ api/urls.py
@@ -3,7 +3,7 @@ from . import views
 from rest_framework.urlpatterns import format_suffix_patterns
 
 urlpatterns = [
-    path("routes", views.getRoutes, name="routes"),
+    path("", views.getRoutes, name="routes"),
     path("getsection", views.get_sections, name="getsections"),
     path("getstudent", views.get_students, name="getstudents"),
     path("getsection/<int:pk>", views.section_api, name="getsection"),
